28416b1e24d4f48de8e517c749b0acc3dadffd0b
Daniel Phillimore <dan@ovms.co>
2015-10-16 15:57:58 +0100

    Updated build

--- dist/dynamic.js
+++ dist/dynamic.js
@@ -12417,6 +12417,12 @@ _.extend(Dynamic.prototype, {
                     onEvent = $element.data('dyn-' + behaviourName + '-on'),
                     options = {
                         get: function (name) {
+                            if (name !== behaviourName) {
+                                // Namespace additional behaviour options,
+                                // so .get('extra') fetches data-dyn-<behav>-extra
+                                name = behaviourName + '-' + name;
+                            }
+
                             return $element.data('dyn-' + name);
                         }
                     };
